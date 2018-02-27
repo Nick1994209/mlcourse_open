42301a853f5c65318894aff97bfb2ad6111c743c
nvkorolkov <NVKorolkov@domclick.ru>
2018-02-27 19:45:37 +0300

    add k_neares_neighbor

--- asd.py
+++ asd.py
@@ -1,5 +1,39 @@
-from sklearn.model_selection import train_test_split
+import numpy as np
 
+a = np.array((
+    [
+        [10, 12, 13],
+        [0, 1, 2],
+    ],
+    [
+        [100, 101, 102],
+        [110, 112, 113],
+    ],
+    [
+        [100, 101, 102],
+        [110, 112, 113],
+    ],
+))
+# print(a.shape)
+#
+# print(a.ravel())
+# print(a.transpose())
 
-print(train_test_split([10, 11, 12,1,1,1,1,21,12,31,1,213,123,], test_size=0.3, random_state=17))
-X_train, X_valid, y_train, y_valid = train_test_split([10, 11, 12,1,1,1,1,21,12,31,1,213,123,], test_size=0.3, random_state=17)
+# print(len(a[0]))
+# print(len(a[0][0]))
+# print(len(a[0]) * len(a[0][0]))
+# print(a.reshape((len(a), len(a[0]) * len(a[0][0]))))
+# print(a.reshape((len(a), -1)))
+# print(a.reshape((a.shape[0], -1)))
+#
+# # print(a[[0, 1]])
+
+print()
+
+print(a.reshape(-1))
+
+print(np.sort(a.reshape(-1)))
+print(np.argsort(a.reshape(-1)))
+print(a.reshape(-1)[np.argsort(a.reshape(-1))])
+
+print(np.argmax(np.bincount([1, 1, 2, 5, 5, 5, 5000004])))
